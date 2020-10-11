7b4d970a8e2c5834ffe887e2bf4e5d1158cb8165
sajinkrishnan <sajinkrishnan@gmail.com>
2020-10-11 20:16:03 +0800

    changes

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -17,7 +17,8 @@ public class LoginTest {
 	@BeforeSuite
 	public void setUp(){
 		
-		WebDriverManager.chromedriver().setup();
+//		WebDriverManager.chromedriver().setup();
+		System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"//chromedriver.exe");
 		driver = new ChromeDriver();
 		driver.manage().window().fullscreen();
 	}
